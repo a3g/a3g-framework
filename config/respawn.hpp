724e06930939a3aaba9d46b90ec6aa3cba64e0b4
Cephei <cepheidev+github@gmail.com>
2015-01-28 00:28:29 +0100

    Added a3g-spectatorscript

--- config/respawn.hpp
+++ config/respawn.hpp
@@ -6,6 +6,7 @@ respawn = "BASE";
 
 
 // Respawn template selection. This decides for example whether or not people can respawn at all, or if they can only JIP.
+// Again, unless you're very sure you want to change these, don't touch them.
 // Can be one of: A3G_Respawn_JipNoRespawn
 
 respawnTemplates[] = {"A3G_Respawn_JipNoRespawn"};
--- initServer.sqf
+++
@@ -1,1 +0,0 @@
-[] execVM "config\general\serverSide.sqf";
