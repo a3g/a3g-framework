ad4b2913b44eb67a1c547a325471d36f0ec8ac01
Cephei <Cephel@users.noreply.github.com>
2015-03-24 12:30:49 +0100

    Update paramsPost.hpp

--- framework/config/paramsPost.hpp
+++ framework/config/paramsPost.hpp
@@ -1,11 +1,11 @@
-class A3G_Template_Spacer1 {
+class A3GFramework_Spacer1 {
   title = "";
   values[] = { 0 };
   texts[] = {""};
   default = 0;
 };
 
-class A3G_Template_Title2 {
+class A3GFramework_Title2 {
   title = "> Custom Parameters:";
   values[] = { 0 };
   texts[] = {""};
