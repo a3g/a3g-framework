3907cde8b3ac932d2dd9d7166f707ef461ff7997
Cephei <Cephel@users.noreply.github.com>
2015-03-31 12:10:12 +0200

    Update cfgDebriefing.hpp

--- framework/config/cfgDebriefing.hpp
+++ framework/config/cfgDebriefing.hpp
@@ -2,7 +2,7 @@ class End1 {
   title = "Mission Success";
   subtitle = "You won";
   pictureBackground = "mission\logo.paa";
-  picture = "mil_circle";
+  picture = "mil_end";
   pictureColor[] = {"(profilenamespace getvariable ['Map_BLUFOR_R',0])","(profilenamespace getvariable ['Map_BLUFOR_G',1])","(profilenamespace getvariable ['Map_BLUFOR_B',1])","(profilenamespace getvariable ['Map_BLUFOR_A',0.8])"};
 };
 
